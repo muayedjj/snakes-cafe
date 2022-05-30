35e2e75e8a666dbe7784c95a7c2394d9ab83e36e
Mu'ayad Al Shareef <103497877+muayedjj@users.noreply.github.com>
2022-05-30 21:47:55 +0300

    comit1

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# snakes-cafe
+## LAB - Class 01
+# Project: # snakes-cafe
+## Author: Muayad Al Shareef
+#### <!-- Links and Resources --> 
+#### <!-- back-end server url (when applicable) --> 
+#### <!-- front-end application (when applicable) --> 
+
+[//]: # (Setup)
+
+[//]: # (.env requirements &#40;where applicable&#41;)
+
+[//]: # (i.e.)
+
+[//]: # ()
+[//]: # (PORT - Port Number)
+
+[//]: # (DATABASE_URL - URL to the running Postgres instance/db)
+
+[//]: # (How to initialize/run your application &#40;where applicable&#41;)
+
+[//]: # (e.g. python main.py)
+
+[//]: # (How to use your library &#40;where applicable&#41;)
+
+[//]: # (Tests)
+
+[//]: # (How do you run tests?)
+
+[//]: # (Any tests of note?)
+
+[//]: # (Describe any tests that you did not complete, skipped, etc)
---
+++ snakes_cafe.py
@@ -0,0 +1,69 @@
+welcome_msg = """ **************************************
+**    Welcome to the Snakes Cafe!   **
+**    Please see our menu below.    **
+**
+** To quit at any time, type "quit" **
+**************************************
+Appetizers
+----------
+Wings
+Cookies
+Spring Rolls
+
+Entrees
+-------
+Salmon
+Steak
+Meat Tornado
+A Literal Garden
+
+Desserts
+--------
+Ice Cream
+Cake
+Pie
+
+Drinks
+------
+Coffee
+Tea
+Unicorn Tears
+
+***********************************
+** What would you like to order? **
+*********************************** """
+
+print(welcome_msg)
+
+menu = {
+    'Wings': 0,
+    'Cookies': 0,
+    'Spring Rolls': 0,
+    'Salmon': 0,
+    'Steak': 0,
+    'Meat Tornado': 0,
+    'A Literal Garden': 0,
+    'Ice Cream': 0,
+    'Cake': 0,
+    'Pie': 0
+}
+order = {}
+
+while True:
+    ord = input("> ")
+    if ord == 'quit':
+        break
+    elif ord not in menu:
+        print("Sorry, your choice isn't available in our menu \n"
+              "** What would you like to order? **")
+    elif ord not in order and ord in menu:
+        order[ord] = 1
+        print(f'** {order[ord]} order of {ord} have been added to your meal **')
+    elif ord in order:
+        order[ord] += 1
+        print(f'** {order[ord]} order of {ord} have been added to your meal **')
+
+if len(order) != 0:
+    print(f"Your order is: {order}, Thank you for your patronage")
+else:
+    print("You didn't order, please visit us again")
